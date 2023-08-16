2a279acbba161f3f851be0a3551221e1e4d0a5c7
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-08-17 01:11:21 +0530

    add. custom utility classes and UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
 "use client";
-import ReactFlow, { Controls, Edge, Node } from "reactflow";
+import "reactflow/dist/style.css";
+
+import ReactFlow, {
+  Controls,
+  Edge,
+  Node,
+  Viewport,
+  ReactFlowProvider,
+} from "reactflow";
 import { useMemo } from "react";
 import ActionNode from "@/nodes/ActionNode";
-import "reactflow/dist/style.css";
 import TriggerNode from "@/nodes/TriggerNode";
 
+const defaultViewPort: Viewport = { x: 0, y: 0, zoom: 0.5 };
+
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { text:undefined}, type:'trigger' },
-  { id: "2", position: { x: 0, y: 100 }, data: { text:undefined} , type:'action'},
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { text: undefined },
+    type: "trigger",
+  },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { text: undefined },
+    type: "action",
+  },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2", type: "step" }];
 
 export default function Home() {
   const nodeTypes = useMemo(
@@ -21,16 +40,16 @@ export default function Home() {
   );
   return (
     <div className="grow">
-      <ReactFlow
-        defaultNodes={initialNodes}
-        defaultEdges={initialEdges}
-        nodeTypes={nodeTypes}
-        fitView
-        attributionPosition="bottom-right"
-
-      >
-        <Controls />
-      </ReactFlow>
+      <ReactFlowProvider>
+        <ReactFlow
+          defaultNodes={initialNodes}
+          defaultEdges={initialEdges}
+          nodeTypes={nodeTypes}
+          fitView
+          attributionPosition="bottom-right"
+          defaultViewport={defaultViewPort}
+        />
+      </ReactFlowProvider>
     </div>
   );
 }
--- nodes/TriggerNode.tsx
+++ nodes/TriggerNode.tsx
@@ -10,33 +10,24 @@ export default function TriggerNode() {
     setSelected(event.target.value);
   };
   return (
-    <div style={{minHeight:'500px',backgroundColor:'pink'}} className="min-h-[500px] bg-cyan-600">
-      <div className="bg-zinc-950">
-        <div
-          style={{
-            background:
-              " linear-gradient(180deg, #E1E0FB 11.46%, #E1ECF7 61.98%)",
-          }}
-          className="font-semibold p-4 min-w-full text-center"
+    <div className="node_container drop_shadow_md">
+      <div className="purple_gradient _heading _flex justify_center" >Trigger</div>
+      <div className="_text _p_8px" >
+        <span className="_mr_8px ">App:</span>
+        <select
+          className="_input"
+          name=""
+          id=""
+          onChange={onSelect}
+          defaultValue="none"
         >
-          Trigger
-        </div>
-        <div className="flex gap-4 items-center">
-          App:
-          <select
-            className="w-[250px] bg-white border border-gray-300 text-gray-700 py-2 px-4 focus:outline-none focus:border-blue-500"
-            name=""
-            id=""
-            onChange={onSelect}
-            defaultValue="none"
-          >
-            <option value="none">None</option>
-            <option value="reddit">Reddit</option>
-            <option value="gmail">Gmail</option>
-          </select>
-        </div>
-        <Handle type="source" position={Position.Bottom} id="a" />
+          <option value="none">None</option>
+          <option value="reddit">Reddit</option>
+          <option value="gmail">Gmail</option>
+        </select>
       </div>
+
+      <Handle type="source" position={Position.Bottom} id="a" />
     </div>
   );
 }
