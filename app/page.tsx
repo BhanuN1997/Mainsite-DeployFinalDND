3ca624a75807200fc139b07346e93b65fc8d146e
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-08-16 19:15:55 +0530

    add. Navbar, Sidebar, Reactflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from 'next/image'
+"use client";
+import ReactFlow, { Controls,Edge,Node } from "reactflow";
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: 'HELLO 1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: 'HELLOW 2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 export default function Home() {
   return (
-   <>Hello world</>
-  )
+    <div className="grow">
+      <ReactFlow  defaultNodes={initialNodes} defaultEdges={initialEdges} fitView >
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
