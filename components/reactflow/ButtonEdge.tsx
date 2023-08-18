110939f4f0e8f8fcf8f781fa0da4a61f9f2edf3d
lioncat2002 <sankhayan2002@gmail.com>
2023-08-18 13:54:53 +0530

    fix: added edge auto connection and working on node positioning

--- components/reactflow/ButtonEdge.tsx
+++ components/reactflow/ButtonEdge.tsx
@@ -33,7 +33,7 @@ export default function ButtonEdge({
 
 
   const {addNode,nodes} = RFStore.getState()
-  const {addEdge,edges}=RFStore.getState()
+  const {removeEdge,addEdge,edges}=RFStore.getState()
 
   const onEdgeClick = (evt: any, id: any) => {
     evt.stopPropagation();
@@ -62,16 +62,9 @@ export default function ButtonEdge({
           <button
             className="edgebutton bg-purple-300 border border-purple-400  p-2 hover:bg-purple-400 focus:outline-none flex justify-center items-center"
             onClick={(event) => {
-              addNode('llm')
+              addNode('default',id)
               console.log(nodes)
-              const newNode:Node=nodes[nodes.length-1]
-              const currEdge:Edge=edges.find(edge=>id===edge.id)
-              const srcNodeId=currEdge.source
-              const targetNodeId=currEdge.target
-
-             addEdge(srcNodeId,newNode.id)
-             addEdge(newNode.id,targetNodeId)
-              console.log(edges)
+             console.log(edges)
             }
           }
           >
--- store/reactFlowStore.ts
+++ store/reactFlowStore.ts
@@ -19,8 +19,9 @@ type RFState = {
   onNodesChange: OnNodesChange;
   onEdgesChange: OnEdgesChange;
   onConnect: OnConnect;
-  addNode: (type: string) => void;
+  addNode: (type: string,id:string) => void;
   addEdge:(sourceId:string,targetId:string)=>void;
+  removeEdge:(id:string)=>void;
 };
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
@@ -42,6 +43,14 @@ const RFStore = create<RFState>((set, get) => ({
       edges: addEdge(connection, get().edges),
     });
   },
+  removeEdge:(id:string)=>{
+    set((state)=>{
+      const data=state.edges.filter(edge=>edge.id!==id)
+      return {
+        edges:data
+      }
+    })
+  },
   addEdge:(sourceId:string,targetId:string)=>{
     set((state)=>{
       const newEdge:Edge={
@@ -56,19 +65,33 @@ const RFStore = create<RFState>((set, get) => ({
       }
     })
   },
-  addNode: (type: string) => {
+  addNode: (type: string,id:string) => {
     set((state) => {
+      const currEdge:Edge=state.edges.find(edge=>id===edge.id)
+      const srcNodeId=currEdge.source
+      const targetNodeId=currEdge.target
+
+      const sourceNode=state.nodes.find(node=>node.id===srcNodeId)
+      let targetNode=state.nodes.find(node=>node.id===targetNodeId)
       const newNode = {
         id: `${state.nodes.length + 1}`,
         data: {
           text: undefined,
         },
-        position: { x: 500, y: state.nodes.length * 500 },
+        position: targetNode.position,
         type: type,
       };
+      targetNode={
+        position: {x:500,y:newNode.position.y+500},
+        ...targetNode
+      }
+      
       console.log(newNode)
-      const data = [...state.nodes,newNode]
-      console.log("addnode called")
+      const data = [...state.nodes.filter(node=>node.id!==targetNode.id),newNode,targetNode]
+      console.log(data)
+      state.removeEdge(currEdge.id)
+      state.addEdge(srcNodeId,newNode.id)
+      state.addEdge(newNode.id,targetNodeId)
       return {
         nodes: data,
       };
