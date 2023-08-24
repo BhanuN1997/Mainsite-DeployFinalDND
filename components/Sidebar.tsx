57551427d283ea9068a084587c8b3fe81764c5b2
lioncat2002 <sankhayan2002@gmail.com>
2023-08-24 17:53:33 +0530

    chore: added ai label

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 const Sidebar = () => {
   return (
     <aside className="flex flex-col gap-16 min-h-[100%] text-2xl p-6 text-gray-600 pt-8 bg-purple-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
+      <div className="flex items-center gap-3 font-bold">
+        Workflow
+      </div>
       <div className="flex items-center gap-3">
         <Image
           src={"/asset/playground.png"}
@@ -32,6 +35,37 @@ const Sidebar = () => {
         Saved Templates
       </div>
 
+      <div className="flex items-center gap-3 font-bold">
+        AI
+      </div>
+
+      <div className="flex items-center gap-3">
+        <Image
+          src={"/asset/playground.png"}
+          alt="Playground image"
+          height={20}
+          width={20}
+        />
+        PlayGround
+      </div>
+      <div className="flex items-center gap-3">
+      <Image
+          src={"/asset/playground.png"}
+          alt="marketplace image"
+          height={30}
+          width={20}
+        />
+        MarketPlace
+      </div>
+      <div className="flex items-center gap-3">
+        <Image
+          src={"/asset/templates.png"}
+          alt="template image"
+          height={20}
+          width={20}
+        />
+        Saved Templates
+      </div>
       
     </aside>
   );
